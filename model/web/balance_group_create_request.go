71f7e4a410e8c3e1c40e6457d8a5a4e43cf0f624
faridjabbar <muhammad.jabbar@vneu.co.id>
2025-07-20 22:05:01 +0700

    deleted

--- model/web/balance_group_create_request.go
+++
@@ -1,9 +0,0 @@
-package web
-
-type BalanceGroupCreateRequest struct {
-	// Fields
-	ID       string `json:"id" validate:"required,min=1,max=20"`
-	Name     string `json:"name"`
-	Type     string `json:"type" validate:"type_debt_and_credit"`
-	Category string `json:"category" validate:"category"`
-}
--- model/web/balance_group_update_request.go
+++
@@ -1,8 +0,0 @@
-package web
-
-type BalanceGroupUpdateRequest struct {
-	// Fields
-	Name     string `json:"name"`
-	Type     string `json:"type"`
-	Category string `json:"category"`
-}
